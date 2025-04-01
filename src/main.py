2ec3a814f327b0fc774f2cbdd5b00a5f9f18cfce
nk-randers <nivethan.kandiah@randers.dk>
2025-04-01 08:43:55 +0200

    update main menu

--- src/main.py
+++ src/main.py
@@ -1,33 +1,50 @@
 import streamlit as st
-from utils.logo import get_logo
 from streamlit_option_menu import option_menu
 from page.conversation_result import show_conversation_result
 from page.conversation_calls import show_conversation_call
-
-st.set_page_config(page_title="Jobcenter", page_icon="assets/favicon.ico", layout="wide")
+from utils.logo import get_logo
 
 with st.sidebar:
     st.sidebar.markdown(get_logo(), unsafe_allow_html=True)
-    selected = option_menu(
+    selected_main = option_menu(
         "Jobcenter",
-        ['Varighed af samtale', 'Resultat af opkald', 'Ventetid pr opkald', 'Antal af samtaler'],
-        icons=['bi-broadcast', 'bi-clock', 'bi-check-circle', 'bi-hourglass-split', 'bi-telephone'],
+        ["Zylinc", "Betjeninger"],
+        icons=["headset", "gear"],
         menu_icon="bi-headset",
         default_index=0,
-        styles={
-            "container": {"padding": "5px", "background-color": "#f0f0f0"},
-            "icon": {"color": "#4a4a4a", "font-size": "18px"},
-            "nav-link": {"font-size": "18px", "text-align": "left", "margin": "0px", "--hover-color": "#e0e0e0"},
-            "nav-link-selected": {"background-color": "#d0d0d0", "color": "#4a4a4a"},
-            "menu-title": {"font-size": "20px", "font-weight": "bold", "color": "#4a4a4a", "margin-bottom": "10px"},
-        }
     )
 
-if selected == 'Varighed af samtale':
-    st.write("## Varighed af samtale")
-elif selected == 'Antal af samtaler':
-    show_conversation_call()
-elif selected == 'Resultat af opkald':
-    show_conversation_result()
-elif selected == 'Ventetid pr opkald':
-    st.write("## Ventetid pr opkald")
+    if selected_main == "Zylinc":
+        selected_zylinc = option_menu(
+            "Zylinc",
+            ['Varighed af samtale', 'Resultat af opkald', 'Ventetid pr opkald', 'Antal af samtaler'],
+            icons=['clock', 'check-circle', 'hourglass-split', 'telephone'],
+            menu_icon="headset",
+            default_index=0,
+        )
+    elif selected_main == "Betjeninger":
+        selected_betjeninger = option_menu(
+            "Betjeninger",
+            ['Kø', 'Ansatte', 'Ressourcestyring'],
+            icons=['list-task', 'people', 'tools'],
+            menu_icon="gear",
+            default_index=0,
+        )
+
+if selected_main == "Zylinc":
+    if selected_zylinc == 'Varighed af samtale':
+        st.write("## Varighed af samtale")
+    elif selected_zylinc == 'Resultat af opkald':
+        show_conversation_result()
+    elif selected_zylinc == 'Ventetid pr opkald':
+        st.write("## Ventetid pr opkald")
+    elif selected_zylinc == 'Antal af samtaler':
+        show_conversation_call()
+
+elif selected_main == "Betjeninger":
+    if selected_betjeninger == 'Kø':
+        st.write("## Kø")
+    elif selected_betjeninger == 'Ansatte':
+        st.write("## Ansatte")
+    elif selected_betjeninger == 'Ressourcestyring':
+        st.write("## Ressourcestyring")
