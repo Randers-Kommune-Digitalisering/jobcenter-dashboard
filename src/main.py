28cdd8e5637e18e4eb8295d6b4a7094a2d7adaa5
Karl Fritjof Krassel <122357806+kf-randers@users.noreply.github.com>
2025-04-03 14:51:56 +0200

    FrontDesk-data added

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 from page.conversation_result import show_conversation_result
 from page.conversation_calls import show_conversation_call
 from page.conversation_queue_time import show_queue_time
+from page.operations import show_operations
 from utils.logo import get_logo
 
 
@@ -12,7 +13,7 @@ with st.sidebar:
     st.sidebar.markdown(get_logo(), unsafe_allow_html=True)
     selected_main = option_menu(
         "Jobcenter",
-        ["Zylinc", "Betjeninger"],
+        ["Zylinc", "FrontDesk"],
         icons=["headset", "gear"],
         menu_icon="bi bi-briefcase-fill",
         default_index=0,
@@ -26,10 +27,10 @@ with st.sidebar:
             menu_icon="headset",
             default_index=0,
         )
-    elif selected_main == "Betjeninger":
+    elif selected_main == "FrontDesk":
         selected_betjeninger = option_menu(
-            "Betjeninger",
-            ['Kø', 'Ansatte', 'Ressourcestyring'],
+            "FrontDesk",
+            ['Betjeninger', 'Ansatte'],
             icons=['list-task', 'people', 'tools'],
             menu_icon="gear",
             default_index=0,
@@ -45,10 +46,9 @@ if selected_main == "Zylinc":
     elif selected_zylinc == 'Antal af samtaler':
         show_conversation_call()
 
-elif selected_main == "Betjeninger":
-    if selected_betjeninger == 'Kø':
-        st.write("## Kø")
+elif selected_main == "FrontDesk":
+    if selected_betjeninger == 'Betjeninger':
+        st.write("## Betjeninger")
+        show_operations()
     elif selected_betjeninger == 'Ansatte':
         st.write("## Ansatte")
-    elif selected_betjeninger == 'Ressourcestyring':
-        st.write("## Ressourcestyring")
---
+++ src/page/operations.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+import math
+import datetime as dt
+from sqlalchemy import create_engine
+
+# # Uncomment when using database
+from utils.config import POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_DATABASE
+db_client = create_engine(f'postgresql+psycopg2://{POSTGRES_DB_USER}:{POSTGRES_DB_PASS}@{POSTGRES_DB_HOST}/{POSTGRES_DB_DATABASE}')
+
+operations = pd.read_sql('SELECT * FROM operationsjobcenter', db_client)
+
+
+def decimalMinutesToStr(minutes):
+    if not minutes:
+        return "—"
+    if math.isnan(minutes):
+        return "—"
+    if isinstance(minutes, str):
+        minutes = float(minutes)
+    minutesOut = int(minutes)
+
+    def minutesText(x):
+        return "minut" if x == 1 else "minutter"
+    secondsOut = (minutes - int(minutes)) * 60
+    if minutesOut == 0:
+        return f"__{secondsOut:.0f}__ sekunder"
+    if secondsOut == 0:
+        return f"__{minutesOut}__ {minutesText(minutesOut)}"
+    return f"__{minutesOut}__ {minutesText(minutesOut)} og __{secondsOut:.0f}__ sekunder"
+
+
+def format_timedelta_in_danish(td):
+    days = td.days
+    seconds = td.seconds
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    parts = []
+    if days > 0:
+        parts.append(f"{days} dage")
+    if hours > 0:
+        parts.append(f"{hours} timer")
+    if minutes > 0:
+        parts.append(f"{minutes} minutter")
+    if seconds > 0:
+        parts.append(f"{seconds} sekunder")
+
+    return ", ".join(parts)
+
+
+def timedelta_to_minutes(td_str):
+    try:
+        parts = td_str.split(", ")
+        total_minutes = 0
+        for part in parts:
+            if "dage" in part:
+                total_minutes += int(part.split()[0]) * 60 * 24
+            elif "timer" in part:
+                total_minutes += int(part.split()[0]) * 60
+            elif "minutter" in part:
+                total_minutes += int(part.split()[0]) * 1
+            elif "sekunder" in part:
+                total_minutes += int(part.split()[0]) / 60
+        return total_minutes
+    except ValueError:
+        return None
+
+
+operations["CreatedAt"] = pd.to_datetime(operations["CreatedAt"])
+operations["CalledAt"] = pd.to_datetime(operations["CalledAt"])
+operations["EndedAt"] = pd.to_datetime(operations["EndedAt"])
+operations["dato"] = pd.to_datetime(operations["CreatedAt"]).dt.date
+operations["year"] = pd.to_datetime(operations["CreatedAt"]).dt.year
+operations["ugenr"] = pd.to_datetime(operations["CreatedAt"]).dt.isocalendar().week
+operations["QueueName"] = operations["QueueName"].str.strip()
+operations["EmployeeName"] = operations["EmployeeName"].str.strip()
+operations["Ventetid"] = (operations["CalledAt"] - operations["CreatedAt"]).apply(format_timedelta_in_danish)
+operations["Betjeningstid"] = (operations["EndedAt"] - operations["CalledAt"]).apply(format_timedelta_in_danish)
+
+currentYear = dt.datetime.now().year
+lastYear = currentYear - 1
+currentWeek = dt.datetime.now().isocalendar().week
+latestWeekOperations = operations[(operations["år"] == currentYear)]["ugenr"].max()
+
+latestDateOperations = operations["CreatedAt"].max()
+latestDateOperations = latestDateOperations.date()
+
+employees = operations["EmployeeName"].unique()
+employees = [str(employee) for employee in employees]
+
+colorScale = alt.Scale(
+    domain=["Betjeninger sidste år", "Betjeninger i år"],
+    range=["#83C9FF", "#0068C9"]
+)
+
+operations = operations[operations["State"] != "Discarded"]
+operations = operations.drop(columns=["MunicipalityID", "QueueId", "State", "StateId", "CounterId", "EmployeeId", "DelayedUntil", "DelayedFrom", "IsEmployeeAnonymized", "EmployeeInitials"])
+
+
+def show_operations():
+    # st.write(operations)
+
+    # Number of operations last year by week
+    dfGraph = operations.groupby(["ugenr", "year"]).size().reset_index(name="antal")
+    dfGraph = dfGraph[dfGraph["year"] >= lastYear]
+
+    dfGraph["operations"] = np.where(dfGraph["year"] == lastYear, dfGraph["antal"], pd.NA)
+    dfGraph["series"] = np.where(dfGraph["year"] == lastYear, "Betjeninger sidste år", pd.NA)
+    dfGraph["operations"] = np.where((dfGraph["year"] == currentYear), dfGraph["antal"], dfGraph["operations"])
+    dfGraph["series"] = np.where((dfGraph["year"] == currentYear), "Betjeninger i år", dfGraph["series"])
+
+    weekly_operations_chart = alt.Chart(dfGraph).mark_line().encode(
+        x=alt.X('ugenr', title='Uge', scale=alt.Scale(domain=[1, dfGraph["ugenr"].max()]), axis=alt.Axis(values=[1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, dfGraph["ugenr"].max()])),
+        y=alt.Y('operations', title='Betjeninger'),
+        color=alt.Color('series', scale=colorScale, legend=alt.Legend(orient='bottom', title=None, labelFontSize=12, titleFontSize=14)),
+        strokeDash=alt.condition(
+            alt.datum.series == 'Forecast i år',
+            alt.value([5, 5]),  # Dashed line for forecast
+            alt.value([0])  # Solid line for others
+        ),
+        strokeWidth=alt.condition(
+            alt.datum.series == 'Forecast i år',
+            alt.value(1),  # Thicker line for forecast
+            alt.value(1)  # Thinner line for others
+        ),
+        tooltip=['ugenr', 'operations']
+    )
+
+    sumOperationsLastYear = dfGraph.loc[(dfGraph["series"] == "Betjeninger sidste år"), "antal"].sum()
+    sumOperationsCurrentYear = dfGraph.loc[(dfGraph["series"] == "Betjeninger i år"), "antal"].sum()
+
+    # Display in Streamlit
+    st.markdown("### Samlet antal betjeninger")
+    st.write(f"Samlet antal betjeninger sidste år: __{sumOperationsLastYear:,.0f}__. Antal betjeninger i år: __{sumOperationsCurrentYear:,.0f}__.".replace(",", "."))
+    st.altair_chart(weekly_operations_chart, use_container_width=True)
+
+    # Betjeningingstid
+    st.markdown("### Betjeningingstid indeværende år")
+
+    # Filter data for the latest year (current year)
+    operationsCurrentYear = operations[operations["year"] == currentYear]
+
+    # Convert "Betjeningstid" to numeric values
+    operationsCurrentYear["Betjeningstid_minutes"] = operationsCurrentYear["Betjeningstid"].apply(timedelta_to_minutes)
+
+    betjeningstidGennemsnit = operationsCurrentYear["Betjeningstid_minutes"].mean()
+
+    # Create a histogram using Altair
+    betjeningstid_histogram = alt.Chart(operationsCurrentYear).mark_bar().encode(
+        x=alt.X('Betjeningstid_minutes:Q', bin=alt.Bin(maxbins=30), title='Betjeningstid (minutter)'),
+        y=alt.Y('count()', title='Betjeninger'),
+        tooltip=['count()']
+    )
+    # Display the histogram in Streamlit
+    st.write(f"Gennemsnitlig ventetid: {decimalMinutesToStr(betjeningstidGennemsnit)}.")
+    st.altair_chart(betjeningstid_histogram, use_container_width=True)
+
+    # Ventetid
+    operationsCurrentYear["Ventetid_minutes"] = operationsCurrentYear["Ventetid"].apply(timedelta_to_minutes)
+
+    ventetidGennemsnit = operationsCurrentYear["Ventetid_minutes"].mean()
+
+    # Create a histogram using Altair
+    Ventetid_histogram = alt.Chart(operationsCurrentYear).mark_bar().encode(
+        x=alt.X('Ventetid_minutes:Q', bin=alt.Bin(maxbins=30), title='Ventetid (minutter)'),
+        y=alt.Y('count()', title='Betjeninger'),
+        tooltip=['count()']
+    )
+    # Display the histogram in Streamlit
+    st.markdown("### Ventetid indeværende år")
+    st.write(f"Gennemsnitlig ventetid: {decimalMinutesToStr(ventetidGennemsnit)}.")
+    st.altair_chart(Ventetid_histogram, use_container_width=True)
